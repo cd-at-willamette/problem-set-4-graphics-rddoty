923879c67d6106569f84659aa63a728a9b24de2b
rddoty <145079678+rddoty@users.noreply.github.com>
2024-10-12 19:24:30 -0700

    Update Prob1.py

--- Prob1.py
+++ Prob1.py
@@ -1,25 +1,46 @@
 ############################################################
-# Name:
-# Name(s) of anyone worked with:
-# Est time spent (hr):
+# Name: Reagan
+# Name(s) of anyone worked with: Jace higa
+# Est time spent (hr): 1 hour
 ############################################################
 
-from pgl import GWindow, GRect, GOval, GLine, GLabel
+from pgl import GWindow, GOval, GRect, GLine, GLabel
 
-# Setting up initial window dimensions. 
-# You can change these if you want. They are in number of pixels.
-WIDTH = 600
-HEIGHT = 600
+GWINDOW_WIDTH = 700
+GWINDOW_HEIGHT = 280
 
 def draw_image():
-    """ Creates a window and draws a student's creation"""
-
-    # Creating the window
-    gw = GWindow(WIDTH, HEIGHT)
-    
-    # And now it is your turn! Add your code below! Make sure you meet all the requirements!
-
-
+    gw = GWindow(GWINDOW_WIDTH, GWINDOW_HEIGHT)
+    red = GRect(0, 0, GWINDOW_WIDTH, GWINDOW_HEIGHT)
+    red.set_color("Red")
+    red.set_filled(True)
+    gw.add(red)
+    orange = GRect(0, 40, GWINDOW_WIDTH, GWINDOW_HEIGHT)
+    orange.set_color("Orange")
+    orange.set_filled(True)
+    gw.add(orange)
+    white = GRect(0, 120, GWINDOW_WIDTH, GWINDOW_HEIGHT)
+    white.set_color("White")
+    white.set_filled(True)
+    gw.add(white)
+    pink = GRect(0, 160, GWINDOW_WIDTH, GWINDOW_HEIGHT)
+    pink.set_color("Pink")
+    pink.set_filled(True)
+    gw.add(pink)
+    magenta = GRect(0, 240, GWINDOW_WIDTH, GWINDOW_HEIGHT)
+    magenta.set_color("Magenta")
+    magenta.set_filled(True)
+    gw.add(magenta)
+    circle = GOval(310, 140, 100, 40)
+    circle.set_color("Black")
+    circle.set_filled(True)
+    gw.add(circle)
+    line = GLine(330, 170, 390, 170)
+    line.set_color("White")
+    gw.add(line)
+    label = GLabel("go lesbians!", 320, 170 )
+    label.set_color("White")
+    gw.add(label)
 
 
 
